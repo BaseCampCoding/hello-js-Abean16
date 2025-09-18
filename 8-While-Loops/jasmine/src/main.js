f1872a9a00c7cb74a8ca79f25debbb11bdd3fb54
Aleah <abean26@basecampcodingacademy.org>
2025-09-18 16:13:35 -0500

    changes

--- 8-While-Loops/jasmine/src/main.js
+++ 8-While-Loops/jasmine/src/main.js
@@ -1,5 +1,5 @@
 
-let juiceName = "orange";
+let juiceName = "Energizer";
 let timeToMixJuiceResult;
 if (juiceName === "Pure Strawberry Joy") {
     timeToMixJuiceResult = 0.5;
@@ -21,25 +21,44 @@ let wedgesNeeded = 20;
 let limes = ["small", "medium", "large"];
 let wedgesCut = 0;
 let limesCut = 0;
+let i = 0;
 
 while (wedgesCut < wedgesNeeded && i < limes.length) {
-    let lime = limes[i];
-
-    if (lime === "small") {
+    if(limes[i] == "small"){
         wedgesCut += 6;
     }
-    else if (lime === "medium") {
+    else if (limes[i] === "medium") {
         wedgesCut += 8;
     }
-    else if (lime === "large") {
+    else if (limes[i] === "large") {
         wedgesCut += 10;
     }
+    limesCut++; 
     i++;
 }
 
-let timeLeft = 20;
-let orders = ["orange", "apple", "grape"]
-for (juice in orders) {
+let timeLeft = 50;
+let orders = ["Pure Strawberry Joy", "Energizer", "Tropical Island", "Green Garden", "All or Nothing"]
+let totalTime = 0;
 
-}
-console.log("anything")
+for(let i = 0; i < orders.length; i++){
+
+    if (juiceName === "Pure Strawberry Joy"){
+        timeToMixJuiceResult = 0.5;
+    }
+    else if (juiceName === "Energizer" || juiceName === "Green Garden"){
+        timeToMixJuiceResult = 1.5;
+    }
+    if (juiceName === "Tropical Island"){
+        timeToMixJuiceResult = 3;
+    }
+    if (juiceName === "All or Nothing"){
+        timeToMixJuiceResult = 5;
+    }
+    else{
+        timeToMixJuiceResult = 2.5;
+    }
+    totalTime += timeToMixJuiceResult;
+} 
+
+console.log(totalTime)
